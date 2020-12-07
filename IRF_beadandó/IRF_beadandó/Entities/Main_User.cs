c394bc78fc80fc4b1267c32300808b3b6e96054e
Ádám Kappéter <adam@kappeterdesign.hu>
2020-12-07 10:20:21 +0100

    osztályok létrehozzása

---
+++ IRF_beadandó/IRF_beadandó/Entities/Main_User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_beadandó.Entities
+{
+    class Main_User
+    {
+        public string nev { get; set; }
+
+        public string jelszo { get; set; }
+
+
+    }
+}
---
+++ IRF_beadandó/IRF_beadandó/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_beadandó.Entities
+{
+    class User
+    {
+        public string nev { get; set; }
+        public string jelszo { get; set; }
+    }
+}
