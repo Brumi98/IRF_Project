4b6f6dea8257e6bcfd50d123289bc402c4763e80
Ádám Kappéter <adam@kappeterdesign.hu>
2020-12-07 10:34:37 +0100

    Jelölt osztály

---
+++ IRF_beadandó/IRF_beadandó/Entities/Candite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_beadandó.Entities
+{
+    class Candite
+    {
+        public string nev { get; set; }
+
+    }
+}
